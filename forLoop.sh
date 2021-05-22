7376e33681bb995908416f7270ddb3c9ba0e8b98
Deepak Yadav <alfo7742@gmail.com>
2021-05-22 11:54:57 +0530

    For loop and renaming file script

--- forLoop.sh
+++ forLoop.sh
@@ -1,5 +1,22 @@
-Names="Brad Kevin Alice Amish Mark Deepak Alex Mark"
+
+#>>> basic for loop
+: 'Names="Brad Kevin Alice Amish Mark Deepak Alex Mark"
 for Name in $Names
 do
 	echo "hello $Name"
 done
+'
+# complete for loop<<<
+
+
+#>>> for loop for renaming files
+files=$(ls *.txt)
+NEW="new"
+for FILE in $files
+do
+	echo "renaming $FILE to new-$FILE"
+	mv $FILE $NEW-$FILE
+done
+
+
+# renaming done using for loop<<<
